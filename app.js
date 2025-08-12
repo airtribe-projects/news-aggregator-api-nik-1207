8a9f11ef78594b9d9bd03f13bdfdd6c6dd8a2446
nik-1207 <nmknikhilkashyap@gmail.com>
2025-08-12 22:58:58 +0530

    refactor: fixes

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-const express = require('express');
+const express = require("express");
+const userRoutes = require("./src/routes/user.routes");
+const newsRoutes = require("./src/routes/news.routes");
+const env = require("dotenv")
+
+env.config();
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/users", userRoutes);
+app.use("/news", newsRoutes);
+
 app.listen(port, (err) => {
-    if (err) {
-        return console.log('Something bad happened', err);
-    }
-    console.log(`Server is listening on ${port}`);
+  if (err) {
+    return console.log("Something bad happened", err);
+  }
+  console.log(`Server is listening on ${port}`);
 });
-
-
 
 module.exports = app;
